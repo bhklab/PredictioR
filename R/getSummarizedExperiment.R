bb57f5e81dc629329affbd3964f0128ef683bd3f
Farnoosh Babazadeh <farnooshbabazadeh@gmail.com>
2023-09-27 16:16:40 -0400

    create SE class from MAE class

---
+++ R/getSummarizedExperiment.R
@@ -0,0 +1,45 @@
+## libraries
+
+library(MultiAssayExperiment)
+library(data.table)
+
+#################################################################
+## Extract expression, clinical, and annotation from MAE class
+#################################################################
+
+getSummarizedExperiment <- function(dat_icb){
+
+  ## slot names for expression data
+  dat_type <- c("expr", "expr_gene_tpm")
+
+  dat_type_ix <- which(dat_type %in% names(dat_icb))
+  expr <- assay(dat_icb[[dat_type[dat_type_ix]]])
+  clin <- as.data.frame(colData(dat_icb[[dat_type[dat_type_ix]]]))
+  annot <- as.data.frame(rowData(dat_icb[[dat_type[dat_type_ix]]]))
+
+  ## limit to protein-coding genes
+  annot <- annot[annot$gene_type == "protein_coding", , drop=FALSE]
+
+  ## remove ENSG*Par_Y
+  remove_Par_Y <- grep("PAR_Y",rownames(annot))
+  if(length(remove_Par_Y) > 0){
+    annot <- annot[-remove_Par_Y, ]
+  }
+
+  ## keep the smallest ENSG for each gene symbol
+  annot <- annot[order(rownames(annot)), , drop=FALSE]
+  annot <- annot[!duplicated(annot$gene_name), , drop=FALSE]
+
+  ## subset gene expression data
+  expr <- expr[rownames(annot), , drop=FALSE]
+  ## change feature names from ENSG ids to gene symbols
+  rownames(expr) <- rownames(annot) <- annot$gene_name
+
+  ## create SummarizedExperiments
+  eset <- SummarizedExperiment(assay= list("gene_expression"=expr),
+                               rowData=annot,
+                               colData=clin)
+
+  return(eset)
+
+}
