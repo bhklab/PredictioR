425e733c4d010659a8ca3551d4bf9b96ecdc855b
Farnoosh Babazadeh <farnooshbabazadeh@gmail.com>
2024-06-03 09:59:49 -0400

    add ICB Liu

---
+++ R/ICB_Liu.R
@@ -0,0 +1,7 @@
+#' ICB Liu Melanoma
+#'
+#' Immune checkpoint blockade (ICB) Liu Melnaoma immunotherapy (PD-1) dataset.
+#'
+#' A SE object with 16,737 protein-coding genes and 74 patients were treated immunotherapy targeted PD-1:
+#' @source <https://pubmed.ncbi.nlm.nih.gov/31792460/>
+"ICB_Liu"
