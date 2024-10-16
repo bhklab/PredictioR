9dd0cd4582097658718951548cd114ea1eb7f49e
Farnoosh Babazadeh <farnooshbabazadeh@gmail.com>
2024-10-16 09:38:57 -0400

    update signature data and signature score computation functions.

---
+++ R/COX_IS_Bonavita.R
@@ -0,0 +1,5 @@
+#' COX-IS Bonavita RNA Signature
+#'
+#' Gene expression signature capturing the pro-tumoral COX Inflammatory activity. This COX Inflammatory Signature (COX-IS) was generated in mouse data and found a predictor of the failure of CTLA-4 and PD-(L)1 blockade in several published cohorts.
+#' @source https://doi.org/10.1016/j.immuni.2020.10.020
+"COX_IS_Bonavita"
